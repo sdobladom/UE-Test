b87be7e459a4c9c4b37dbb7b92565da7f6fea305
sdobladom <sdoblado@minsati.com>
2025-07-29 13:04:02 +0200

    Fix

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -4,7 +4,7 @@ export default async function decorate(block) {
   const tags = block.querySelectorAll('[data-aue-resource]');
   const elements = Array.from(tags).map(el => el.getAttribute('data-aue-resource'));
   const data = elements[0].split("aemconnection:/")
-  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[0] + ".json"
+  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[1] + ".json"
   console.log(url)
   const content = await ffetch(url).all();
   console.log(content)
