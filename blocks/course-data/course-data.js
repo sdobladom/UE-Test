ebf8e7699518d47b603fa2a6096208e74800ea2f
sdobladom <sdoblado@minsati.com>
2025-07-29 13:08:33 +0200

    Fix

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -6,6 +6,19 @@ export default async function decorate(block) {
   const data = elements[0].split("aemconnection:/")
   const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[1] + ".json"
   console.log(url)
-  const content = await ffetch(url).all();
-  console.log(content)
+  const content = await fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error al obtener: ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log('Contenido del componente:', data);
+    return data;
+  })
+  .catch((error) => {
+    console.error('Error en fetch:', error);
+    return null;
+  });
 }
