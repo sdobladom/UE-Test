64b700610e8634029a2d2a0d39c3ba0009f7f934
sdobladom <sdoblado@minsati.com>
2025-07-29 13:01:32 +0200

    Fix

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -2,7 +2,8 @@ import ffetch from '../../scripts/ffetch.js';
 
 export default function decorate(block) {
   const elements = block.querySelectorAll('[data-aue-resource]');
-  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + elements[0].split("aemconnection:/")[0] + ".json"
+  const data = elements[0].split("aemconnection:/")
+  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[0] + ".json"
   console.log(url)
   //const content = await ffetch(url).all();
   console.log(content)
