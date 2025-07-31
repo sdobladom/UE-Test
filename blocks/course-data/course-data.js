c3fdac7f11a6cd93c8f4fd633d8d0ff1c384f361
sdobladom <sdoblado@minsati.com>
2025-07-31 11:13:09 +0200

    Fix

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -1,12 +1,12 @@
 import ffetch from '../../scripts/ffetch.js';
 
-export default async function decorate(block) {
+export default function decorate(block) {
   const tags = block.querySelectorAll('[data-aue-resource]');
   const elements = Array.from(tags).map(el => el.getAttribute('data-aue-resource'));
   const data = elements[0].split("aemconnection:/")
   const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[1] + ".json"
   console.log(url)
-  const content = await fetch(url)
+  const content = fetch(url)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Error al obtener: ${response.statusText}`);
