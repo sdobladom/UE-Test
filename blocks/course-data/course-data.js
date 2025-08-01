e3a2257b3994948520e5e26482c2e90c3229c537
sdobladom <sdoblado@minsati.com>
2025-08-01 12:55:11 +0200

    Form test

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -23,15 +23,21 @@ export default function decorate(block) {
 //  });
 
   const paragraphs = document.querySelectorAll('p');
-  const code = +paragraphs[paragraphs.length -1].textContent
+  const codeP = paragraphs[paragraphs.length -1]
+  const code = +codeP.textContent
 
-  block.append(
+   console.log(code)
+
+  const container = codeP.parentElement;
+  container.innerHTML = ''
+
+  container.append(
     form({ id: `mktoForm_${code}` })
   )
 
-  block.append(
+  container.append(
     script({ src: '//402-ALY-118.mktoweb.com/js/forms2/js/forms2.min.js', onload: () => {
-      MktoForms2.loadForm("//402-ALY-118.mktoweb.com", "402-ALY-118", 1002);
+      MktoForms2.loadForm("//402-ALY-118.mktoweb.com", "402-ALY-118", code);
     }})
   )
 }
