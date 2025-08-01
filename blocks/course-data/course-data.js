efbb91735e071ed7fcd1a7249d32cce5d3f0fc57
sdobladom <sdoblado@minsati.com>
2025-08-01 08:36:14 +0200

    Revert "Fix"
    This reverts commit c3fdac7f11a6cd93c8f4fd633d8d0ff1c384f361.

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -1,35 +1,24 @@
-import { form, script } from '../../scripts/dom-helpers.js';
+import ffetch from '../../scripts/ffetch.js';
 
-export default function decorate(block) {
-//  const tags = block.querySelectorAll('[data-aue-resource]');
-//  const elements = Array.from(tags).map(el => el.getAttribute('data-aue-resource'));
-//  const data = elements[0].split("aemconnection:/")
-//  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[1] + ".json"
-//  console.log(url)
-//  const content = fetch(url)
-//  .then((response) => {
-//    if (!response.ok) {
-//      throw new Error(`Error al obtener: ${response.statusText}`);
-//    }
-//    return response.json();
-//  })
-//  .then((data) => {
-//    console.log('Contenido del componente:', data);
-//    return data;
-//  })
-//  .catch((error) => {
-//    console.error('Error en fetch:', error);
-//    return null;
-//  });
-
-  block.innerHTML = '';
-  block.append(
-    form({ id: "mktoForm_1002" })
-  )
-  
-  block.append(
-    script({ src: '//402-ALY-118.mktoweb.com/js/forms2/js/forms2.min.js', onload: () => {
-      MktoForms2.loadForm("//402-ALY-118.mktoweb.com", "402-ALY-118", 1002);
-    }})
-  )
+export default async function decorate(block) {
+  const tags = block.querySelectorAll('[data-aue-resource]');
+  const elements = Array.from(tags).map(el => el.getAttribute('data-aue-resource'));
+  const data = elements[0].split("aemconnection:/")
+  const url = "https://author-p147864-e1510969.adobeaemcloud.com/" + data[1] + ".json"
+  console.log(url)
+  const content = await fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Error al obtener: ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log('Contenido del componente:', data);
+    return data;
+  })
+  .catch((error) => {
+    console.error('Error en fetch:', error);
+    return null;
+  });
 }
