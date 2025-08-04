eab2e8a9a25ac3cb29ab74dcb9aa2cfa716d32d5
sdobladom <sdoblado@minsati.com>
2025-08-04 08:23:39 +0200

    UPD: changed components structure

--- blocks/course-data/course-data.js
+++ blocks/course-data/course-data.js
@@ -2,16 +2,18 @@ import { loadBlock } from '../../scripts/aem.js';
 
 
 export default function decorate(block) {
-  const ps = block.querySelectorAll('p')
-  for (const el of ps) {
-    if (!Number.isNaN(+el.textContent)) {
-      const parent = el.parentElement
-      parent.classList.add('block', 'form');
-      parent.dataset.blockName = 'form';
-      parent.textContent = el.textContent
-      el.remove()
-      loadBlock(parent)
-      break
-    }
-  }
+//  const ps = block.querySelectorAll('p')
+//  for (const el of ps) {
+//    if (!Number.isNaN(+el.textContent)) {
+//      const parent = el.parentElement
+//      parent.classList.add('block', 'form');
+//      parent.dataset.blockName = 'form';
+//      parent.textContent = el.textContent
+//      el.remove()
+//      loadBlock(parent)
+//      break
+//    }
+//  }
+
+
 }
