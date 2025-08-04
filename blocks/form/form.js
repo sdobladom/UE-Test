2e0ebf6e32990d226379a087b135c4ff4cccffbe
sdobladom <sdoblado@minsati.com>
2025-08-04 08:52:05 +0200

    UPD: form script changed

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -2,23 +2,18 @@ import { form, script } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
 
-//  const paragraphs = document.querySelectorAll('.form');
-//  const codeP = paragraphs[paragraphs.length -1]
-//  console.log(codeP.textContent)
-//  const code = +codeP.textContent
-//
-//  console.log(code)
-//
-//  const container = codeP.parentElement;
-//  container.innerHTML = ''
-//
-//  container.append(
-//    form({ id: `mktoForm_${code}` })
-//  )
-//
-//  container.append(
-//    script({ src: '//402-ALY-118.mktoweb.com/js/forms2/js/forms2.min.js', onload: () => {
-//      MktoForms2.loadForm("//402-ALY-118.mktoweb.com", "402-ALY-118", code);
-//    }})
-//  )
+  const container = document.querySelectorAll('.form div > div')[0];
+  const code = +container.children[0].textContent
+
+  container.innerHTML = ''
+
+  container.append(
+    form({ id: `mktoForm_${code}` })
+  )
+
+  container.append(
+    script({ src: '//402-ALY-118.mktoweb.com/js/forms2/js/forms2.min.js', onload: () => {
+      MktoForms2.loadForm("//402-ALY-118.mktoweb.com", "402-ALY-118", code);
+    }})
+  )
 }
